974e055743ea1006818d09dcc88d4ebff9dafce0
up887818 <57417766+up887818@users.noreply.github.com>
2019-11-05 23:01:07 +0000

    05/11/19 Saving from Desktop
    Worked on Introduction Activity. Need to find way to include counter in onClick() method.

--- app/src/main/java/com/example/up887818/Introduction.java
+++ app/src/main/java/com/example/up887818/Introduction.java
@@ -1,5 +1,7 @@
 package com.example.up887818;
 
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Introduction extends AppCompatActivity {
@@ -19,8 +22,33 @@ public class Introduction extends AppCompatActivity {
         setContentView(R.layout.activity_introduction);
 
         String name = getIntent().getStringExtra("name");
-        TextView test = (TextView)findViewById(R.id.textView);
-        test.setText(String.format("Hi %s", name));
+        Resources res = getResources();
+
+        final TextView test = findViewById(R.id.textView);
+        final Button b = findViewById(R.id.button);
+        final String[] strings = res.getStringArray(R.array.intro_explaination);
+
+        int counter = 0;
+
+        b.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+
+                if (counter < 4) {
+                    counter++;
+                    test.setText(strings[counter]);
+                    if (counter == 3) {
+                        b.setText("Let's go!");
+                    }
+                } else {
+                    Intent i = new Intent(Introduction.this, Test.class);
+                    i.putExtra("name", name);
+                    startActivity(i);
+                }
+            }});
+
+        test.setText(String.format(strings[0], name));
+
+
 
         //Todo: Create introduction to story.
     }
